33d7fc1ba1bed260b430e4deff3e2295d99e3068
Alexander Skalozubov <skalozubov@gmail.com>
2013-04-21 04:37:33 +0300

    Added date and time fragments.

---
+++ src/org/android/app/locationreminder/fragments/TimePickerFragment.java
@@ -0,0 +1,26 @@
+package org.android.app.locationreminder.fragments;
+
+import java.util.Calendar;
+
+import org.android.app.locationreminder.activities.AddReminderActivity;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+
+public class TimePickerFragment extends DialogFragment {
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+	// Use the current time as the default values for the picker
+	final Calendar c = Calendar.getInstance();
+	int hour = c.get(Calendar.HOUR_OF_DAY);
+	int minute = c.get(Calendar.MINUTE);
+	
+	// Create a new instance of TimePickerDialog and return it
+	return new TimePickerDialog(getActivity(), (AddReminderActivity)getActivity(), hour, minute, DateFormat.is24HourFormat(getActivity()));
+	}
+
+}
